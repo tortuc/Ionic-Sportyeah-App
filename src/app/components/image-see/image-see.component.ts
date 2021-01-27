f30407a1e72d8c0ae151487cc726213152cffc89
David Valor <davidvalorwork@gmail.com>
2021-01-27 12:59:17 -0430

    Arregladas las imagenes en products

---
+++ src/app/components/image-see/image-see.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-image-see',
+  templateUrl: './image-see.component.html',
+  styleUrls: ['./image-see.component.scss'],
+})
+export class ImageSeeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
