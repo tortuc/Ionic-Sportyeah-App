516ec5a35792a551ff50565f74aa1250eec30bbe
David Valor <davidvalorwork@gmail.com>
2021-03-04 13:04:29 -0430

    Link y modal de retar

--- src/app/components/challenges/modal-created/modal-created.component.ts
+++ src/app/components/challenges/modal-created/modal-created.component.ts
@@ -1,3 +1,4 @@
+import { ToastController } from '@ionic/angular';
 import { environment } from "src/environments/environment";
 import { IChallenge } from "./../../../service/challenge.service";
 import { UserService } from "./../../../service/user.service";
@@ -15,12 +16,12 @@ export class ModalCreatedComponent implements OnInit {
   users: any[] = null;
   name: string = "";
 
-  constructor(public userService: UserService) {}
+  constructor(public userService: UserService, public toast: ToastController) {}
 
   ngOnInit() {
     console.log(this.Challenge);
     this.users = this.userService.followings;
-    this.challengeLink = `${environment.URL_FRONT}challenge/${this.Challenge._id}`;
+    this.challengeLink = `${environment.URL_FRONT}challenge/${this.userService.User.username}/${this.Challenge._id}`;
   }
 
   onChange(search) {
@@ -28,5 +29,24 @@ export class ModalCreatedComponent implements OnInit {
     this.users = this.users.filter(
       (user: any) => JSON.stringify(user).indexOf(search) !== -1
     );
+  }
+  async copyLink() {
+    let selBox = document.createElement("textarea");
+    selBox.style.position = "fixed";
+    selBox.style.left = "0";
+    selBox.style.top = "0";
+    selBox.style.opacity = "0";
+    selBox.value = this.challengeLink;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand("copy");
+    document.body.removeChild(selBox);
+    const toast = await this.toast.create({
+      message:"Copiado",
+      cssClass: "centerToast",
+      duration: 2000
+    })
+    await toast.present()
   }
 }
