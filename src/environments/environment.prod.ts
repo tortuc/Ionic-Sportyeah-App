82d3ad448e6e656e8d7df03260ac66785f54b9c9
Jogeiker Lizarraga <jogeiker1999@gmail.com>
2021-03-19 15:30:36 -0400

    api socket

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   URL_FRONT: "https://app.sportyeah.com/",
   URL_API: "https://api.sportyeah.com/v1",
   URL_IMAGE: "https://files.sportyeah.com/v1",
-  URL_SOCKET: "https://www.sportyeah.com",
+  URL_SOCKET: "https://api.sportyeah.com",
   URL_CHALLENGE: "https://challange.sportyeah.com",
 };
