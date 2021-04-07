dd7de39517b73d4d835c6df64f68f81da3d16eee
David Valor <davidvalorwork@gmail.com>
2021-04-07 17:38:23 -0430

    arreglado

--- src/app/challenges/profile-challenge/profile-challenge.page.ts
+++ src/app/challenges/profile-challenge/profile-challenge.page.ts
@@ -1,7 +1,7 @@
 import { take } from 'rxjs/operators';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { ChallengeService } from 'src/app/service/challenge.service';
+import { ChallengeService, IChallenge } from 'src/app/service/challenge.service';
 import { UserService } from 'src/app/service/user.service';
 import { IUserDataResponse } from 'src/app/models/IUserDataResponse';
 import { IUser } from 'src/app/models/IUser';
@@ -15,12 +15,13 @@ import { ReusableComponentsIonic } from 'src/app/service/ionicHelpers.service';
 export class ProfileChallengePage implements OnInit {
   user: IUser = null
   noUser: boolean = false
+  challenges: IChallenge[] = null
 
   constructor(
     public userService: UserService,
     public route: ActivatedRoute,
     public challengeService: ChallengeService,
-    public reusableCI: ReusableComponentsIonic
+    public reusableCI: ReusableComponentsIonic,
   ) { }
 
   ngOnInit() {
@@ -28,9 +29,20 @@ export class ProfileChallengePage implements OnInit {
 
   ionViewWillEnter(){
     this.userService.getUserByUsername(this.route.snapshot.params.username)
-      .pipe(take(1)).subscribe((userData: IUserDataResponse)=>
-        this.user = userData.user,err=> this.noUser = true
+      .pipe(take(1)).subscribe((userData: IUserDataResponse)=>{
+        this.user = userData.user
+        this.getChallenges()
+      },err=> this.noUser = true
       )
+  }
+
+  getChallenges(){
+    this.challengeService.getUserChallenge(this.user._id)
+      .pipe(take(1))
+      .subscribe((response:any)=>{
+        this.challenges = response.challenges
+        console.log(response)
+      })
   }
 
 }
