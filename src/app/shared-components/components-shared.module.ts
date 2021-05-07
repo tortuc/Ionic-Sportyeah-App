fac9d289c4465a4aa068c00e35065c06564e1b7c
Jogeiker Lizarraga <jogeiker1999@gmail.com>
2021-05-07 17:25:36 +0200

    componets shared

--- src/app/shared-components/components-shared.module.ts
+++ src/app/shared-components/components-shared.module.ts
@@ -6,23 +6,27 @@ import { TranslateModule } from "@ngx-translate/core";
 import { PipesModule } from "../pipes/pipes.module";
 import { LangButtonComponent } from "./lang-button/lang-button.component";
 import { LangBtnAppComponent } from "./lang-btn-app/lang-btn-app.component";
-// import { FollowButtonComponent } from "../components/follow-button/follow-button.component";
+import { FollowButtonComponent } from "../components/follow-button/follow-button.component";
 import { MainCardUserComponent } from "./main-card-user/main-card-user.component";
 import { RouterModule } from "@angular/router";
 import { ViewSponsorsComponent } from "./view-sponsors/view-sponsors.component";
 import { LoadingProgressComponent } from "./loading-progress/loading-progress.component";
 import { AttachLinkPreviewPostComponent } from "../post-components/attach-link-preview-post/attach-link-preview-post.component";
 import { UrlPreviewComponent } from "../components/url-preview/url-preview.component";
+import { FriendsMobileComponent } from "../components/friends-mobile/friends-mobile.component";
+import { PopularGroupsPanelComponent } from "../components/popular-groups-panel/popular-groups-panel.component";
 
 const components = [
-  // FollowButtonComponent,
+  FollowButtonComponent,
   LangButtonComponent,
   LangBtnAppComponent,
   MainCardUserComponent,
   ViewSponsorsComponent,
   LoadingProgressComponent,
   AttachLinkPreviewPostComponent,
-  UrlPreviewComponent
+  UrlPreviewComponent,
+  PopularGroupsPanelComponent,
+  FriendsMobileComponent
 ];
 
 @NgModule({
