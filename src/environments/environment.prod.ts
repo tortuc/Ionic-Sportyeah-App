1a160481989a7d1f20ebb5c563d305d90a7edb64
Juan Claudio Gudiño <juanclaudiogud@hotmail.com>
2021-05-07 15:21:48 -0300

    Add environment interface

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,11 @@
-export const environment = {
+import { Environment } from './interfaces';
+
+export const environment: Environment = {
   production: true,
   URL_FRONT: "https://app.sportyeah.com/",
   URL_API: "https://api.sportyeah.com/v1",
   URL_IMAGE: "https://files.sportyeah.com/v1",
   URL_SOCKET: "https://api.sportyeah.com",
   URL_CHALLENGE: "https://challenge.sportyeah.com",
-  URL_WEB:'https://app.sportyeah.com'
-
+  URL_WEB:'https://app.sportyeah.com',
 };
