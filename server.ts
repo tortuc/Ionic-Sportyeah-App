f085ffc5d5d548e890110fcfec5c2804cc858dbd
jogeiker <jogeiker1999@gmail.com>
2021-06-17 10:50:34 -0500

    join

--- server.ts
+++ server.ts
@@ -7,7 +7,7 @@ import 'zone.js/dist/zone-node';
 
 import { ngExpressEngine } from '@nguniversal/express-engine';
 import * as express from 'express';
-import * as path from "path"
+import {join}] from "path"
 import { AppServerModule } from './src/main.server';
 import { APP_BASE_HREF } from '@angular/common';
 import { existsSync } from 'fs';
