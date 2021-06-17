624be77aa282f959b90140e3608cc8035cd659e7
jogeiker <jogeiker1999@gmail.com>
2021-06-17 10:30:11 -0500

    test index server

--- server.ts
+++ server.ts
@@ -23,7 +23,7 @@ global['navigator'] = win.navigator;
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
   const server = express();
-  const distFolder = join(process.cwd(), 'dist/app/browser');
+  const distFolder = join(process.cwd(), '/../browser');
   const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
 
   // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
