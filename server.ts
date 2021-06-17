4d9d929c4540a7901d9b2a72785c876da22f7a63
jogeiker <jogeiker1999@gmail.com>
2021-06-17 11:11:52 -0500

    win.self

--- server.ts
+++ server.ts
@@ -3,48 +3,58 @@ import "localstorage-polyfill";
 global["localStorage"] = localStorage;
 global["sessionStorage"] = require("sessionstorage");
 
-import 'zone.js/dist/zone-node';
+import "zone.js/dist/zone-node";
 
-import { ngExpressEngine } from '@nguniversal/express-engine';
-import * as express from 'express';
-import {join} from "path";
-import { AppServerModule } from './src/main.server';
-import { APP_BASE_HREF } from '@angular/common';
-import { existsSync } from 'fs';
+import { ngExpressEngine } from "@nguniversal/express-engine";
+import * as express from "express";
+import { join } from "path";
+import { AppServerModule } from "./src/main.server";
+import { APP_BASE_HREF } from "@angular/common";
+import { existsSync } from "fs";
+const distFolder = join(process.cwd(), "/../browser");
 
-const domino = require('domino');
-const indexRoute = join(process.cwd(), '/../browser/index.html')
-const win = domino.createWindow( indexRoute);
-global['window'] = win;
-global['self'] = win;
-global['document'] = win.document;
-global['navigator'] = win.navigator;
+const domino = require("domino");
+const indexHtml = existsSync(join(distFolder, "index.original.html"))
+  ? "index.original.html"
+  : "index";
 
+const win = domino.createWindow(indexHtml);
+global["window"] = win;
+global["self"] = win.self;
+global["document"] = win.document;
+global["navigator"] = win.navigator;
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
   const server = express();
-  const distFolder = join(process.cwd(), '/../browser');
-  const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
 
   // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
-  server.engine('html', ngExpressEngine({
-    bootstrap: AppServerModule,
-  }));
+  server.engine(
+    "html",
+    ngExpressEngine({
+      bootstrap: AppServerModule,
+    })
+  );
 
-  server.set('view engine', 'html');
-  server.set('views', distFolder);
+  server.set("view engine", "html");
+  server.set("views", distFolder);
 
   // Example Express Rest API endpoints
   // server.get('/api/**', (req, res) => { });
   // Serve static files from /browser
-  server.get('*.*', express.static(distFolder, {
-    maxAge: '1y'
-  }));
+  server.get(
+    "*.*",
+    express.static(distFolder, {
+      maxAge: "1y",
+    })
+  );
 
   // All regular routes use the Universal engine
-  server.get('*', (req, res) => {
-    res.render(indexHtml, { req, providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }] });
+  server.get("*", (req, res) => {
+    res.render(indexHtml, {
+      req,
+      providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }],
+    });
   });
 
   return server;
@@ -55,8 +65,7 @@ function run(): void {
 
   // Start up the Node server
   const server = app();
-  server.listen(port, () => {
-  });
+  server.listen(port, () => {});
 }
 
 // Webpack will replace 'require' with '__webpack_require__'
@@ -64,9 +73,9 @@ function run(): void {
 // The below code is to ensure that the server is run only when not requiring the bundle.
 declare const __non_webpack_require__: NodeRequire;
 const mainModule = __non_webpack_require__.main;
-const moduleFilename = mainModule && mainModule.filename || '';
-if (moduleFilename === __filename || moduleFilename.includes('iisnode')) {
+const moduleFilename = (mainModule && mainModule.filename) || "";
+if (moduleFilename === __filename || moduleFilename.includes("iisnode")) {
   run();
 }
 
-export * from './src/main.server';
+export * from "./src/main.server";
