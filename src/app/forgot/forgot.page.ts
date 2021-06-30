73087492fce5d5aa4ee96ea64c13d94102047d29
jogeiker <jogeiker1999@gmail.com>
2021-06-30 15:06:31 -0500

    loading

--- src/app/forgot/forgot.page.ts
+++ src/app/forgot/forgot.page.ts
@@ -1,72 +1,64 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { TranslateService } from '@ngx-translate/core';
-import { LoginService } from '../service/login.service';
-import { UserService } from '../service/user.service';
-import { AlertController, LoadingController, PopoverController } from '@ionic/angular';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { TranslateService } from "@ngx-translate/core";
+import { LoginService } from "../service/login.service";
+import { UserService } from "../service/user.service";
+import { AlertController } from "@ionic/angular";
+import { LoadingService } from "../service/loading.service";
 
-interface resp {
-  code:Number
-  user:Object
-  err:Object
-}
 @Component({
-  selector: 'app-forgot',
-  templateUrl: './forgot.page.html',
-  styleUrls: ['./forgot.page.scss'],
+  selector: "app-forgot",
+  templateUrl: "./forgot.page.html",
+  styleUrls: ["./forgot.page.scss"],
 })
 export class ForgotPage implements OnInit {
-
   constructor(
-    private fb:FormBuilder,
-    private loginService:LoginService,
-    private alertController:AlertController,
-    private translate:TranslateService,
-    private router:Router,
-    public userService:UserService,
-    private loadingCtrl:LoadingController,
-  ) { }
+    private fb: FormBuilder,
+    private loginService: LoginService,
+    private alertController: AlertController,
+    private translate: TranslateService,
+    private router: Router,
+    public userService: UserService,
+    private loadingService: LoadingService
+  ) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   forgotForm = this.fb.group({
-    email:['',[Validators.email,Validators.required]]
-  })
+    email: ["", [Validators.email, Validators.required]],
+  });
 
-  async send(){
-    let loading = await this.loadingCtrl.create({
-      message:this.translate.instant('loading')
-    });
-    loading.present();
-    
-    this.loginService.forgot(this.forgotForm.value).toPromise()
-    .then(async (result)=>{
-      loading.dismiss()
-      
-      let alert = await this.alertController.create({
-      
-        header: this.translate.instant("forgot.success.title"),
-        message: this.translate.instant("forgot.success.msg"),
-        buttons: [this.translate.instant("forgot.success.button")]
+  async send() {
+    this.loadingService.present();
+
+    this.loginService
+      .forgot(this.forgotForm.value)
+      .toPromise()
+      .then(async (result) => {
+        this.loadingService.dismiss();
+
+        let alert = await this.alertController.create({
+          header: this.translate.instant("forgot.success.title"),
+          message: this.translate.instant("forgot.success.msg"),
+          buttons: [this.translate.instant("forgot.success.button")],
+        });
+
+        await alert.present();
+        this.router.navigate(["/login"], {
+          preserveFragment: false,
+          replaceUrl: true,
+        });
+      })
+      .catch(async (err) => {
+        this.loadingService.dismiss();
+        let alert = await this.alertController.create({
+          header: this.translate.instant("forgot.wrong.title"),
+          message: this.translate.instant("forgot.wrong.msg"),
+          buttons: [this.translate.instant("forgot.wrong.button")],
+        });
+
+        await alert.present();
       });
-  
-      await alert.present();
-      this.router.navigate(["/login"],{preserveFragment:false,replaceUrl:true})
-    })
-    .catch(async (err)=>{
-       
-      loading.dismiss()
-      let alert = await this.alertController.create({
-        header: this.translate.instant("forgot.wrong.title"),
-        message: this.translate.instant("forgot.wrong.msg"),
-        buttons: [this.translate.instant("forgot.wrong.button")]
-      });
-  
-      await alert.present();
-    })
-   
   }
-
 }
